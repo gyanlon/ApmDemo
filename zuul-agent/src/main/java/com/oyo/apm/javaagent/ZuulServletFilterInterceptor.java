51f2dcef956d75e83cba8bb19c5a2379b00befb9
gyanlon <gyanlon@hotmail.com>
2019-02-27 17:06:33 +0800

    Add servlet interceptor

--- zuul-agent/src/main/java/com/oyo/apm/javaagent/ZuulServletFilterInterceptor.java
+++ zuul-agent/src/main/java/com/oyo/apm/javaagent/ZuulServletFilterInterceptor.java
@@ -1,17 +1,34 @@
 package com.oyo.apm.javaagent;
 
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.Callable;
 
 public class ZuulServletFilterInterceptor {
 
     @RuntimeType
-    public static Object intercept(@SuperCall Callable<?> zuper) throws Exception {
+    public static Object intercept(@SuperCall Callable<?> zuper, @AllArguments Object... args) throws Exception {
         System.out.println("intercept：拦截了");
 
-        return zuper.call();
+
+        if (args.length >= 1 && args[0] instanceof HttpServletRequest) {
+            HttpServletRequest req = (HttpServletRequest) args[0];
+            System.out.println(req.getRequestURL());
+        }
+
+        Object call = zuper.call();
+
+        if (args.length >= 2 && args[1] instanceof HttpServletResponse) {
+            HttpServletResponse res = (HttpServletResponse) args[1];
+            System.out.println(res.getStatus());
+
+        }
+
+        return call;
     }
 
 }
